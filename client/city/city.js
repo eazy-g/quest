b74394384422f25eac65a3a0cf39bb0aa01c92f2
eazy-g <gbrustkern@gmail.com>
2016-01-06 15:02:07 -0600

    Home page refactored. nav bar added

--- client/city/city.js
+++ client/city/city.js
@@ -1,12 +1,36 @@
 angular.module('cityQuest.city', [])
 
-.controller('cityCtrl', function($scope, $location, $window, QuestStorage, Auth){
+.controller('cityCtrl', function($scope, $location, $window, QuestStorage, InputConversion, Auth){
   $scope.city = "";
+  $scope.signedIn = !!$window.localStorage.getItem('sessiontoken');
+
+  $scope.signout = function(){
+    Auth.signout();
+  };
 
   $scope.citySelect = function(){
     QuestStorage.saveCity($scope.city.toLowerCase());
     $location.path('/questList');
   };
+
+  //This will be refactored later to get two random, top rated quests
+  var getTwoRandomQuests = function(){
+    QuestStorage.getAllQuests()
+    .then(function(quests){
+      var questsFound = quests.length > 0;
+      if(questsFound){
+        quests.forEach(function(quest){
+          quest.time = InputConversion.minutesToHours(quest.time);
+        });
+        $scope.quests = quests;
+      }else{
+        $scope.showNoQuestsFoundMsg = true;
+      }
+    })
+    .catch(function(error){
+      console.log(error);
+    });
+  }
 
   // var sessionCheck = function(){
   //   if( ! Auth.isAuth()) {
@@ -15,4 +39,7 @@ angular.module('cityQuest.city', [])
   // }
 
   // sessionCheck();
+  getTwoRandomQuests();
+  $window.localStorage.removeItem('city');
+  $window.localStorage.removeItem('coords');
 });
