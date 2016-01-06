27d3759d34705046a8109be695a2c84170c08beb
Ashley Augustini <avaugustini@gmail.com>
2016-01-05 19:48:09 -0600

    Rebasing

--- client/createQuest/createQuest.js
+++ client/createQuest/createQuest.js
@@ -95,21 +95,6 @@ angular.module('cityQuest.createQuest', [])
     }
   };
 
-  // var addressField = document.getElementById('search_address');
-  // var geocoder = new google.maps.Geocoder();
-  // function search() {
-  //   geocoder.geocode(
-  //     {'address': addressField.value},
-  //     function(results, status) {
-  //       if (status === google.maps.GeocoderStatus.OK) {
-  //         var loc = results[0].geometry.location;
-  //       } else {
-  //         alert("Not found: " + status);
-  //       }
-  //     }
-  //   );
-  // };
-
   var loadGoogleMapAndListenForMarkerClicks = function(){
     uiGmapGoogleMapApi.then(function(maps) {
     $scope.markers = [];
